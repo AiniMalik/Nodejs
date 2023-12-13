f3e4a2507bea0627725734ab9fe3063c13dcd50d
Qurat-ul-ain <30411856+AiniMalik@users.noreply.github.com>
2023-12-13 22:06:49 +0500

    Update config.js

--- db/config.js
+++ db/config.js
@@ -1,5 +1,3 @@
 const mongoose = require("mongoose");
 mongoose.connect("mongodb://localhost:27017/s-administration");
 
-// mongoose.connect(mongodb+srv://AinMalik:<password>@cluster0.xsmm1rs.mongodb.net/test)
-
